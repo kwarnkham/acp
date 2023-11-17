14265716f8400655044989f1e271515778370b16
Moon <kwarnkham.moon@gmail.com>
2023-11-17 17:37:36 +0630

    update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 clean up files in s3
 
 ```
-docker run \
+docker run --rm \
   -p 6001:6001 \
   -p 9601:9601 \
   -e SOKETI_DEBUG=1 \
--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -64,8 +64,9 @@ namespace App\Models{
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \App\Models\Round $round
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Round> $rounds
- * @property-read int|null $rounds_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Round> $tickets
+ * @property-read int|null $tickets_count
+ * @property-read \App\Models\User $user
  * @method static \Illuminate\Database\Eloquent\Builder|Order filter(array $filters)
  * @method static \Illuminate\Database\Eloquent\Builder|Order newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Order newQuery()
@@ -82,6 +83,31 @@ namespace App\Models{
  * @method static \Illuminate\Database\Eloquent\Builder|Order whereUserId($value)
  */
 	class Order extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\PaymentMethod
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $number
+ * @property string $account_name
+ * @property int $status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod query()
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereAccountName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod whereUpdatedAt($value)
+ */
+	class PaymentMethod extends \Eloquent {}
 }
 
 namespace App\Models{
@@ -151,6 +177,8 @@ namespace App\Models{
  *
  * @property int $id
  * @property int $item_id
+ * @property string|null $ticket_id
+ * @property int $status
  * @property int $max_tickets
  * @property int $price_per_ticket
  * @property int $price
@@ -163,6 +191,8 @@ namespace App\Models{
  * @property-read int|null $order_details_count
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Order> $orders
  * @property-read int|null $orders_count
+ * @property-read \App\Models\Ticket|null $ticket
+ * @method static \Illuminate\Database\Eloquent\Builder|Round filter(array $filters)
  * @method static \Illuminate\Database\Eloquent\Builder|Round newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Round newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Round query()
@@ -174,9 +204,36 @@ namespace App\Models{
  * @method static \Illuminate\Database\Eloquent\Builder|Round whereNote($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Round wherePrice($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Round wherePricePerTicket($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Round whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Round whereTicketId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Round whereUpdatedAt($value)
  */
 	class Round extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Ticket
+ *
+ * @property int $id
+ * @property int $order_id
+ * @property int $round_id
+ * @property int $code
+ * @property float $price
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket whereOrderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket whereRoundId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket whereUpdatedAt($value)
+ */
+	class Ticket extends \Eloquent {}
 }
 
 namespace App\Models{
@@ -192,6 +249,8 @@ namespace App\Models{
  * @property mixed|null $password
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $display_name
+ * @property string|null $telegram_chat_id
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
  * @property-read int|null $notifications_count
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Order> $orders
@@ -205,6 +264,7 @@ namespace App\Models{
  * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|User query()
  * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereDisplayName($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereFbId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereFbName($value)
@@ -212,6 +272,7 @@ namespace App\Models{
  * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereTelegramChatId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
  */
 	class User extends \Eloquent {}
