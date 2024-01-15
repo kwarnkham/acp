5613d7603965b93171009f7f55c466ff6aea612b
Moon <kwarnkham.moon@gmail.com>
2024-01-15 11:09:10 +0630

    allow http://localhost:8080 to pass cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [config('app.frontend_url')],
+    'allowed_origins' => [config('app.frontend_url'), 'http://localhost:8080'],
 
     'allowed_origins_patterns' => [],
 
